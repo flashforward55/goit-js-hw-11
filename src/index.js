5fccbb4ed11ed6c4f63029f8ee9b3663c0fa9cfb
Anton Styazhkin <rudana089@gmail.com>
2023-04-06 23:53:59 +0300

    Update files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('Ok');
+import Notiflix from 'notiflix';
+import axios from 'axios';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox';
+
+const API_KEY = '35140926-fd12774c1839d7d0854ca625c';
+const BASE_URL = `https://pixabay.com/api/?key=${API_KEY}`;
+
+const searchForm = document.querySelector('#search-form');
+const galleryDiv = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more');
