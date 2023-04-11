c5a3c6d5c26ba5ae1b5bc7b58c4c81c197987b6a
Anton Styazhkin <rudana089@gmail.com>
2023-04-11 23:18:32 +0300

    Update notify.js

--- src/js/notify.js
+++ src/js/notify.js
@@ -1,28 +1,47 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
+const notiflixOptions = {
+  width: '330px',
+  fontSize: '16px',
+  position: 'right-top',
+  borderRadius: '4px',
+  timeout: 2500,
+  cssAnimationDuration: 600,
+  cssAnimationStyle: 'from-right',
+};
+
 // Notify functions
 function searchError() {
   Notify.failure(
-    'Sorry, there are no images matching your search query. Please try again.'
+    'Sorry, there are no images matching your search query. Please try again.',
+    notiflixOptions
   );
 }
 
 function searchSucces(imagesSearchQuery) {
-  Notify.success(`Hooray! We found ${imagesSearchQuery} images`);
+  Notify.success(
+    `Hooray! We found ${imagesSearchQuery} images`,
+    notiflixOptions
+  );
 }
 
 function searchResultEnd() {
-  Notify.info("We're sorry, but you've reached the end of search results.");
+  Notify.info(
+    "We're sorry, but you've reached the end of search results.",
+    notiflixOptions
+  );
 }
 
 function errrorStringEmpy() {
   Notify.failure(
-    'The search string cannot be empty. Please specify your search query'
+    'The search string cannot be empty. Please specify your search query',
+    notiflixOptions
   );
 }
 
 function errorCatchImages() {
-  Notify.failure('Error fetching images. Please try again later.');
+  Notify.failure('Error fetching images. Please try again later.'),
+    notiflixOptions;
 }
 
 export {
